b5be45cdc01a180bdc75eb3e24f4b567f6c78a6e
PHoogestraat <68931021+PHoogestraat@users.noreply.github.com>
2021-01-10 17:03:58 -0500

    update csv paths

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,22 @@
 // @TODO: YOUR CODE HERE!
+console.log("test");
+var rawDatax = d3.csv("assets/data/data.csv")
+console.log(rawDatax)
+
+d3.csv("assets/data/data.csv").then(dataRaw => {
+        console.log(dataRaw)
+});
+
+// Load Data
+d3.csv("assets/data/data.csv")
+    .then(res => {
+        console.log(res)
+    })
+
+d3.csv("data.csv").then(function(tvData) {
+    console.log(tvData);
+
+}).catch(function(error) {
+console.log(error);
+});
+      
