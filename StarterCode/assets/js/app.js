60f77df5d3c21a2963fd716428e79c269ef3b111
PHoogestraat <68931021+PHoogestraat@users.noreply.github.com>
2021-01-10 22:18:42 -0500

    plot works

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,22 +1,29 @@
 // @TODO: YOUR CODE HERE!
-console.log("test");
-var rawDatax = d3.csv("assets/data/data.csv")
-console.log(rawDatax)
+console.log("D3 Homework");
 
+
+
+// Load data
 d3.csv("assets/data/data.csv").then(dataRaw => {
-        console.log(dataRaw)
-});
+    console.log(dataRaw)
+    
+    var state = dataRaw.map(data => data.state);
+    console.log("State:", state);
+    
+    var poverty = dataRaw.map(data => data.poverty);
+    console.log("Poverty:", poverty);
+    
+    var obesity = dataRaw.map(data => data.obesity);
+    console.log("Obesity:", obesity);
 
-// Load Data
-d3.csv("assets/data/data.csv")
-    .then(res => {
-        console.log(res)
-    })
+/*
+    // Cast the hours value to a number for each piece of tvData
+    data.forEach(function(data) {
+        console.log("state:" data.state);
+    });
+*/
 
-d3.csv("data.csv").then(function(tvData) {
-    console.log(tvData);
 
 }).catch(function(error) {
-console.log(error);
+    console.log(error);
 });
-      
