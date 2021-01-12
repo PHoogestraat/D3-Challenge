b2ffb754911fceaf5ef8e41b854c0678727f5cb2
PHoogestraat <68931021+PHoogestraat@users.noreply.github.com>
2021-01-12 14:47:00 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 // @TODO: YOUR CODE HERE!
 console.log("D3 Homework");
 
+ 
 
 //*****************************************************
 // set the dimensions and margins of the graph
-var svgWidth = 960;
-var svgHeight = 500;
+var svgWidth = 880;
+var svgHeight = 475;
 
 var margin = {
   top: 20,
@@ -27,7 +28,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data +++++++++++++++++++++++++++
-d3.csv("assets/data/data.csv").then(function(raw) {
+d3.csv("assets/data/data.csv").then(function(raw){ 
     console.log("raw")
     console.log(raw)
 
@@ -37,14 +38,14 @@ d3.csv("assets/data/data.csv").then(function(raw) {
         
         // x axis
         data.poverty = +data.poverty;
-        data.obesity = +data.obesity;
-        obesityLow = +data.obesityLow
+        obesity = +data.obesity;
+        obesityLow = +data.obesityLow 
         obesityHigh = +data.obesityHigh
         smokes = +data.smokes
         smokesLow = +data.smokesLow
         smokesHigh = +data.smokesHigh
         //y axis
-        
+        income = +data.income
         data.healthcare = +data.healthcare;
         healthcareLow = +data.healthcareLow
         healthcareHigh = +data.healthcareHigh
@@ -54,19 +55,27 @@ d3.csv("assets/data/data.csv").then(function(raw) {
         //console.log(data.healthcare);
         
         // needed for bubble label
-        //console.log(data.state)
+        //console.log(data.abbr)
     });
+    
+
+
+
+
 
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(raw, d => d.poverty)])
+      // - 1 shifts scale plot axis
+      .domain([d3.min(raw, d => d.poverty) -1, d3.max(raw, d => d.poverty)])
+    
+    // Leave alone
       .range([0, width]);
         
 
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(raw, d => d.healthcare)])
+      .domain([d3.min(raw, d => d.healthcare) -4, d3.max(raw, d => d.healthcare)])
       .range([height, 0]);
 
 
@@ -87,7 +96,8 @@ d3.csv("assets/data/data.csv").then(function(raw) {
       .call(leftAxis);
 
     // Step 5: Create Circles
-    // ==============================
+    //* ==============================
+    
     var circlesGroup = chartGroup.selectAll("circle")
     .data(raw)
     .enter()
@@ -96,24 +106,77 @@ d3.csv("assets/data/data.csv").then(function(raw) {
     .attr("cy", d => yLinearScale(d.healthcare))
     .attr("r", "15")
     .attr("fill", "blue")
-    .attr("text", d => d.state)
+    //.attr("text", d => d.state)
     .attr("opacity", ".5");
- 
-    // State labels ????????
-    //console.log(d.state)
+    
+    
+
+    // New effort to label bubbles with state abreviation (effort 1/12/2011 #1)
+
+      var statename = chartGroup.append("g")
+      var statetext = statename.selectAll("text")
+          .data(raw)
+          .enter()
+          .append("text")
+          .text(d=>d.abbr) // labels state abreviation
+          .attr("x", d=> xLinearScale(d.poverty))
+          .attr("y", d=> yLinearScale(d.healthcare))
+          .attr("font-size", "10px")
+          .style("fill", "white")
+          .attr("text-anchor", "middle");
+
+
+    // Short code for inserting state abreviation into bubble effort 1
+    /*var cicrleText = circlesGroup.append('text').text(d => d.abbr)
+        .attr("dx", d => xLinearScale(d.poverty))
+        .attr("dy", d => yLinearScale(d.healthcare))
+        .attr("fill", "red")
+        .attr("font-size", "10px")
+    */
+    // Long version code for inserting state abreviation into bubble effort 1
+    // Not needed /*
+    
+    // var textCircles = chartGroup.selectAll("circle")
+    //   .selectAll("text")  // Select text to bind
+    //   .data(raw)
+    //   .enter()
+    //   .append("text")
+    //   .text(d => d.abbr)
+      
+    //   //.attr("dx", 9.2)
+    //   //.attr("dy", 11.4)
+
+    //   .attr("dx", d => xLinearScale(d.poverty))
+    //   .attr("dy", d => yLinearScale(d.healthcare))
+    //   //.attr("dx", d => xLinearScale(9.2))
+    //   //.attr("dy", d => yLinearScale(11.4))
+      
+    //   .attr("font-family", "sans-serif")
+    //   .attr("text-anchor", "middle")
+    //   .attr("font-size", "20px")
+    //   .style("fill", "red")
+    //   .attr("font-weight", "bold");
+    
+
     
     // Step 6: Initialize tool tip
     // ==============================
     var toolTip = d3.tip()
       .attr("class", "tooltip")
-      .offset([80, -60])
+      .offset([80, -80])
       .html(function(d) {
-        return (`${d.state}<br>Poverty%: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
+        return (`<u>${d.state}</u> 
+        <br>Age (ave): ${d.age} 
+        <br>Income (ave$): ${d.income} 
+        <br>Smokes (%): ${d.smokes} 
+        <br>Obesity (%): ${d.obesity}
+        <br>Poverty (%): ${d.poverty}
+        <br>Healthcare (%): ${d.healthcare} `);
       });
 
     // Step 7: Create tooltip in the chart
     // ==============================
-    chartGroup.call(toolTip);
+    circlesGroup.call(toolTip);
 
     // Step 8: Create event listeners to display and hide the tooltip
     // ==============================
@@ -124,6 +187,9 @@ d3.csv("assets/data/data.csv").then(function(raw) {
       .on("mouseout", function(data, index) {
         toolTip.hide(data);
       });
+
+
+
    
     // Create axes labels
     chartGroup.append("text")
@@ -132,12 +198,17 @@ d3.csv("assets/data/data.csv").then(function(raw) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
+      .attr("font-size", "30px")
+      .style("fill", "green")
       .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
+      .attr("font-size", "30px")
+      .style("fill", "green")
       .text("In Poverty (%)");
+      
   }).catch(function(error) {
     console.log(error);
   });
